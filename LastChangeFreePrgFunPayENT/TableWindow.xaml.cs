24eeeee683b742896e9cc3d2b51127f29548217b
PROSTOMAX <tmaximus27122005@gmail.com>
2024-04-03 14:09:08 +0300

    Add project files.

---
+++ LastChangeFreePrgFunPayENT/TableWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using LastChangeFreePrgFunPayENT;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LastChangeFreePrgFunPayENT
+{
+    /// <summary>
+    /// Interaction logic for TableWindow.xaml
+    /// </summary>
+    public partial class TableWindow : Window
+    {
+        LastChangeFourPrgFunPayEntities entfun = new LastChangeFourPrgFunPayEntities();
+        public TableWindow()
+        {
+            InitializeComponent();
+            AllInfaFunPay.ItemsSource = entfun.LastChangeBaseViewData.ToList();
+        }
+
+    
+    }
+}
